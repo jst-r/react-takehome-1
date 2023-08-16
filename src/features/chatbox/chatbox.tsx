fffc74287f6d8f9abcd9e9c644b7124005103eac
Dmitrii Lavrov <d.lavrov@proscom.ru>
2023-08-16 18:40:35 +0200

    Implement and style the input

--- src/features/chatbox/chatbox.tsx
+++ src/features/chatbox/chatbox.tsx
@@ -1,3 +1,4 @@
+import { type SyntheticEvent } from "react";
 import s from "./chatbox.module.scss";
 
 const longText =
@@ -6,10 +7,13 @@ const longText =
 export function Chatbox() {
   return (
     <div className={s.chatbox_container}>
-      <MessageBox text="Test message" sender="user" />
-      <MessageBox text="Test response" sender="bot" />
-      <MessageBox text={longText} sender="user" />
-      <MessageBox text={longText} sender="bot" />
+      <div className={s.messages_container}>
+        <MessageBox text="Test message" sender="user" />
+        <MessageBox text="Test response" sender="bot" />
+        <MessageBox text={longText} sender="user" />
+        <MessageBox text={longText} sender="bot" />
+      </div>
+      <MessageInput onSendMessage={console.log} />
     </div>
   );
 }
@@ -24,5 +28,34 @@ function MessageBox({ text, sender }: MessageBoxProps) {
     <div className={s.message_box} data-sender={sender}>
       {text}
     </div>
+  );
+}
+
+interface MessageInputProps {
+  onSendMessage: (msg: string) => void; // Uncontrolled компонент чтобы не ререндерить всего родителя
+}
+
+function MessageInput({ onSendMessage }: MessageInputProps) {
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.currentTarget as unknown as typeof e.target & {
+      message: { value: string };
+    };
+    const message = target.message.value;
+    e.currentTarget.reset();
+    onSendMessage(message);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={s.message_input_container}>
+      <input
+        type="text"
+        required
+        placeholder="Start typing here..."
+        name="message"
+        className={s.message_input}
+      />
+      <button type="submit" className={s.message_submit_button} />
+    </form>
   );
 }
