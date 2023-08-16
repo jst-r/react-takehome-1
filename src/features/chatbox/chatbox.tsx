38a3e5f5b6cf5b20035cbe749600e95b2a0d0277
Dmitrii Lavrov <d.lavrov@proscom.ru>
2023-08-16 17:39:53 +0200

    styles for long messages

--- src/features/chatbox/chatbox.tsx
+++ src/features/chatbox/chatbox.tsx
@@ -1,10 +1,15 @@
 import s from "./chatbox.module.scss";
 
+const longText =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias laborum excepturi hic asperiores, esse velit quasi! Impedit officiis cum eos aliquam beatae doloremque quo ipsum ratione perferendis sapiente, perspiciatis laboriosam?";
+
 export function Chatbox() {
   return (
     <div className={s.chatbox_container}>
       <MessageBox text="Test message" sender="user" />
       <MessageBox text="Test response" sender="bot" />
+      <MessageBox text={longText} sender="user" />
+      <MessageBox text={longText} sender="bot" />
     </div>
   );
 }
